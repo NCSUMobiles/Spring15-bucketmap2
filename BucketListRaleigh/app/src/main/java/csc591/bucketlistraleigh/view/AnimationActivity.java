986f887ceb221ba5da2f334a4d702dacad4402f8
tmuthuk <tmuthuk@ncsu.edu>
2015-03-17 13:43:40 -0400

    Initial Commit with GIF logo

---
+++ BucketListRaleigh/app/src/main/java/csc591/bucketlistraleigh/view/AnimationActivity.java
@@ -0,0 +1,32 @@
+package csc591.bucketlistraleigh.view;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import csc591.bucketlistraleigh.R;
+
+public class AnimationActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        InputStream stream = null;
+
+        try{
+            stream = getAssets().open("bucket_list_intro.gif");
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+        //GifMoviewView view = new GifMoviewView(this,stream);
+        GifWebView view = new GifWebView(this,"file:///android_asset/intro.html");
+        setContentView(view);
+    }
+}
