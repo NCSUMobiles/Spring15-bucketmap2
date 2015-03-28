1dced232a8c175b23a00d2b66e3c48552e3ad1b6
tmuthuk <tmuthuk@ncsu.edu>
2015-03-27 22:41:34 -0400

    Basic image load

--- BucketListRaleigh/app/src/main/java/csc591/bucketlistraleigh/view/HomeActivity.java
+++ BucketListRaleigh/app/src/main/java/csc591/bucketlistraleigh/view/HomeActivity.java
@@ -1,41 +1,44 @@
 package csc591.bucketlistraleigh.view;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.graphics.Bitmap;
+import android.graphics.BitmapRegionDecoder;
+import android.graphics.Rect;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.app.Fragment;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import csc591.bucketlistraleigh.ImageFragment;
 import csc591.bucketlistraleigh.R;
 
 
-public class HomeActivity extends ActionBarActivity {
+public class HomeActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new ImageFragment()).commit();
         }
 
-        return super.onOptionsItemSelected(item);
+        Log.i("Home activity","You are here in onCreate");
     }
+
+
+
+
 }
